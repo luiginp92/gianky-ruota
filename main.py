3fcddae32d3f5482e423422077d7a8872635526a
simone <srivarola104@gmail.com>
2025-02-27 14:13:51 +0100

    Test minimale: main.py con startup event e route '/'

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+import logging
 
 app = FastAPI()
+logging.basicConfig(level=logging.INFO)
+
+@app.on_event("startup")
+async def startup_event():
+    logging.info("FastAPI startup event triggered.")
 
 @app.get("/")
 async def read_root():
+    logging.info("Root endpoint '/' called.")
     return {"message": "Hello, World!"}
